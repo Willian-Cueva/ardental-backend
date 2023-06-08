989cb9d83924d918bb19f5cc85dfd9eb5c654eed
Willian-Cueva <enriquewillian2@gmail.com>
2023-06-08 18:16:39 -0500

    agregando catch al nodemailer que dejó de funcionar

--- src/mailer.js
+++ src/mailer.js
@@ -5,13 +5,15 @@ const transporter = nodemailer.createTransport({
   port: 465,
   secure: true,
   auth: {
+    // type: "OAuth2",
     user: "enriquewillian2@gmail.com",
     pass: "mksozfcbxpqyqmon",
+    // pass: "frlhstixrqfdvyrl",
   },
 });
 
 transporter.verify().then(() => {
   console.log("Ready for send emails");
-});
+}).catch((e)=>console.log("no se pudo conectar con el servicio de nodemailer"));
 
 module.exports = transporter;
