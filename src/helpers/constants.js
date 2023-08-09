c14991e14313af3b459c300e290eaf74b9dc9532
Willian-Cueva <enriquewillian2@gmail.com>
2023-08-08 22:17:27 -0500

    develop

--- src/helpers/constants.js
+++ src/helpers/constants.js
@@ -1,7 +1,11 @@
 const constants = {}
 
-constants.URL = "https://willian-cueva.github.io/ardental/"
-// constants.URL = "http://localhost:3000/ardental"
+// constants.URL = "https://willian-cueva.github.io/ardental/"
+/**
+ * aki es mas necesario ejecutarlo con la maquina en la que vas a estar habriendo el frontend xdxd pero no influye mucho 
+ * solo sirve mas para recuperar la contraseña
+ */
+constants.URL = "http://localhost:3000/ardental"
 
 constants.URL_RESQUE_PASSWORD = `${constants.URL}#/resq/`
 
