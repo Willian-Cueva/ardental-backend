ca2da42f1d44161dff8ec8568b86dd3476a0dc13
Willian Cueva <enriquewillian2@gmail.com>
2024-03-14 15:38:56 -0500

    Actualizacion de inicio automático 2

--- 3001.bat
+++ 3001.bat
@@ -1,10 +1,1 @@
-@Echo off
-:home
-cls
-cd C:\server\ardental-backend
-git pull origin main
-@npm install
-@npm run dev
-goto End
-REM pm2 --version
-REM npm install pm2 -g
+git pull origin main & npm install & npm run dev
