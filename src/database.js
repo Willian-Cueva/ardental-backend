018968c14a92215212dd555f1868c5a7351f5d32
Willian Cueva <enriquewillian2@gmail.com>
2025-05-03 00:37:16 -0500

    Actualización de plan de tratamientos y pagos

--- src/database.js
+++ src/database.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-const URI = process.env.MONGO_URI || "mongodb://localhost/clinica";
+const URI = process.env.MONGO_URI || "mongodb://127.0.0.1:27017/ardental-back";
 
 console.log(URI);
 
